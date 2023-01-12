c347f185bb077033be491055920e32949ed056b9
kirsah1248 <122039836+kirsah1248@users.noreply.github.com>
2023-01-11 21:39:25 -0500

    sprint 1

--- Final Project copy 2.py
+++ Final Project copy 2.py
@@ -72,7 +72,9 @@ class Meal:
 
 meal_db = shelve.open("meals")
 meal_db["0"] = Meal("Channa", ["lunch", "dinner"], ["chole", "turmeric", "zeera"])
-meal_db["1"] = Meal("Dosa", ["lunch", "dinner"], ["dosa mix", "masala"])
+meal_db["1"] = Meal("Dosa", ["lunch", "dinner", "breakfast"], ["dosa mix", "masala"])
+meal_db["2"] = Meal("gol guppa", ["breakfast"], ["masala","aloo"])
+meal_db["3"] = Meal("pohaa", ["breakfast", "lunch"], ["turmeric", "rice"])
 
 def add_meal():
     name = input("meal name:\n")
@@ -93,17 +95,58 @@ def add_meal():
         except:
             print("seperate using a space and a comma")
             categ_add()
+    
     ingredients = ingre_add()
     categories = categ_add()
     meal_db[str(len(meal_db))] = Meal(name, ingredients, categories)
         
+def meal_generation(day_nums, db):
+    temp_db = db
+    i = 0
+    meal_list = []
+    while i in range(day_nums):
+        x = str(random.randint(0, len(db)-1))
+        if x in temp_db:
+            gen_meal = temp_db[x]
+            del temp_db[x]
+            meal_list.append(gen_meal)
+            i += 1
+        else:
+            continue
+    return meal_list
 
+def search(tags, name_ret):
+    tags = tags.split(", ")
+    meal_keys = []
+    meal_val = []
+    if tags != []:
+        for x in meal_db:
+            if set(tags) <= set(meal_db[x].category + meal_db[x].ingredients):
+                meal_keys.append(x)
+    if name_ret == True:
+        for y in meal_keys:
+            meal_val.append(meal_db[y].name)
+        return meal_val
+    return meal_keys
 
+def filter_generation(tags, day_nums):
+    ml_vals = search(tags, False)
+    i = 0
+    meal_list = []
+    if day_nums <= len(ml_vals):
+        while i in range(day_nums):
+            x = str(random.randint(0, len(ml_vals)-1))
+            gen_meal = meal_db[ml_vals[int(x)]]
+            ml_vals.pop(int(x))
+            meal_list.append(gen_meal.name)
+            i += 1
+    else:
+        return "not enough meals with those tags"
+    return meal_list
+    
 
+    
 def main():
-    a = meal_db[str(random.randint(0, len(meal_db)-1))]
-    a.info()
-    add_meal()
-
+    print(filter_generation("turmeric, lunch", 2))
     
 main()
